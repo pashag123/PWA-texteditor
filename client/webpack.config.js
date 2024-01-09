9807e57a02b8b01834bca6f6b2fbbb83a5285835
pashag123 <pashag123@gmail.com>
2024-01-09 15:30:18 -0800

    more

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,26 +6,58 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+
 module.exports = () => {
   return {
     mode: 'development',
+
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text Editor'
       }),
+
+
+
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'text editor',
+        short_name: 'editor',
+        description: 'a new text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js'
+      })
     ],
 
+
+
     module: {
-      
+
       rules: [
         {
           test: /\.css$/i,
@@ -34,7 +66,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          
+
           use: {
             loader: 'babel-loader',
             options: {
